520dd8baafe49b817bcb650b2bac671fddaf0d4c
bcrespi <tomeu_crespi25@hotmail.com>
2016-03-11 13:50:40 +0100

    master: wip threading

--- telegram_integrations/telegram_bot.py
+++ telegram_integrations/telegram_bot.py
@@ -1,6 +1,7 @@
-from telegram import Updater
-from telegram_integration.telegram_keys import TelegramKeys
-from telegram_integration.telegram_photo import TelegramPhoto
+import threading, time
+from telegram_integrations import Updater
+from telegram_integrations.telegram_keys import TelegramKeys
+from telegram_integrations.telegram_photo import TelegramPhoto
 
 # global variables
 keys = TelegramKeys
@@ -18,14 +19,21 @@ dispatcher.addTelegramCommandHandler('start', start)
 def echo(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
     if TelegramPhoto.is_photo(update):
-        keys.add_key(chat_id=update.message.chat_id, file_id= TelegramPhoto.get_file_id(update))
+        # create thread
+        thr = threading.Thread(target=time_out, args=[bot, update.message.chat_id])
+        # add key in keys
+        keys.add_key(bot=bot, chat_id=update.message.chat_id, file_id= TelegramPhoto.get_file_id(update))
     else:
         bot.sendMessage(chat_id=update.message.chat_id, text="No podemos procesar el siguiente archivo")
-
-
 
 # add @msg to dispatcher
 dispatcher.addTelegramMessageHandler(echo)
 
 # start bot
 updater.start_polling()
+
+# define time out function (threading)
+def time_out(bot, chat_id):
+    # config time expiration
+    time.sleep(120)
+    bot.sendMessage(chat_id=chat_id, text="Ha agotado el tiempo de espera")
--- telegram_integrations/telegram_keys.py
+++ telegram_integrations/telegram_keys.py
@@ -1,23 +1,26 @@
+import threading
+
 class TelegramKeys:
+
+    lock = threading.Lock()
 
     def __init__(self):
         self.keys = {}
 
-    def add_key(self, chat_id, file_id):
-        if chat_id in self.keys:
-            self.keys[chat_id] += [file_id]
-        else:
-            self.keys[chat_id] = [file_id]
-            # create and start thread
-            threading.Thread(target=time_out, args=[bot, update.message.chat_id]).start()
+    def add_key(self, bot, chat_id, file_id):
+        self.lock.acquire()
+        try:
+            if chat_id in self.keys:
+                self.keys[chat_id] += [file_id]
+            else:
+                self.keys[chat_id] = [file_id]
+        finally:
+            self.lock.release()
 
     def remove_key(self, chat_id):
-        if chat_id in self.keys:
-            del self.keys[chat_id]
-
-    # define time out function (threading)
-    def time_out(bot, chat_id):
-        # config time expiration
-        time.sleep(5)
-        KEYS.expiration()
-        bot.sendMessage(chat_id=chat_id, text="Ha agotado el tiempo de espera")
+        self.lock.acquire()
+        try:
+            if chat_id in self.keys:
+                del self.keys[chat_id]
+        finally:
+            self.lock.release()
