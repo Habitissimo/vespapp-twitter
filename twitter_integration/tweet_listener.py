0d20b4b63893ee9722e5c99343ba694b793763a6
JoanMartin <joanmartinmiralles@hotmail.com>
2016-03-12 10:06:48 +0100

    contact for API added

--- twitter_integration/tweet_listener.py
+++ twitter_integration/tweet_listener.py
@@ -20,7 +20,8 @@ class TweetListener(tweepy.StreamListener):
             payload = {
                 'type': 0,
                 'source': 'twitter',
-                'free_text': tweet['text'].encode('ascii', 'ignore')
+                'free_text': tweet['text'].encode('ascii', 'ignore'),
+                'contact': tweet['user']['screen_name']
             }
             # Optional arguments
             if tweet['place']:
@@ -29,21 +30,22 @@ class TweetListener(tweepy.StreamListener):
 
             # Create sighting
             response = requests.post(self.url + '/sightings/', data=payload)
-            sighting = json.loads(response.text)
-            sighting_id = sighting['id']
+            if response.status == 201:
+                sighting = json.loads(response.text)
+                sighting_id = sighting['id']
 
-            # Reply the tweet
-            nick_name = tweet['user']['screen_name']
-            text = 'Gracias por subir la foto'
-            in_reply_to_status_id = tweet['id_str']
-            self.tweet.reply(nick_name, text, in_reply_to_status_id)
+                # Reply the tweet
+                nick_name = tweet['user']['screen_name']
+                text = 'Gracias por subir la foto'
+                in_reply_to_status_id = tweet['id_str']
+                self.tweet.reply(nick_name, text, in_reply_to_status_id)
 
-            # Add photos
-            for media_object in tweet['extended_entities']['media']:
-                file_path = media_object['media_url_https']
-                open_file = urlopen(file_path)
-                bytes = open_file.read()
-                requests.post(self.url + '/sightings/' + str(sighting_id) + '/photos/', files={'file': bytes})
+                # Add photos
+                for media_object in tweet['extended_entities']['media']:
+                    file_path = media_object['media_url_https']
+                    open_file = urlopen(file_path)
+                    bytes = open_file.read()
+                    requests.post(self.url + '/sightings/' + str(sighting_id) + '/photos/', files={'file': bytes})
 
         return True
 
